ec54acbc70e4e354583c70a599e0b8f4356388e8
Michał Mazur <michal2707@o2.pl>
2025-03-24 23:02:36 +0100

    Initial commit

---
+++ src/main/java/pl/cyrkoniowa/centrumdiety/CentrumDietyApplication.java
@@ -0,0 +1,13 @@
+package pl.cyrkoniowa.centrumdiety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentrumDietyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentrumDietyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/cyrkoniowa/centrumdiety/CentrumDietyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.cyrkoniowa.centrumdiety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentrumDietyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
